0cab13fddbeccf4cbf4c1dcc8203144adb1a6ed9
Tommy Pham <phamtommylong1991@gmail.com>
2019-10-23 11:05:36 -0700

    skip link

--- src/components/better/route-target-heading.js
+++ src/components/better/route-target-heading.js
@@ -1,38 +1,43 @@
-import React from "react"
-import { css } from "@emotion/core"
+import React from "react";
+import { css } from "@emotion/core";
 
 const styles = css`
-    .routeSkipHeading {
-        position: relative;
+  .routeSkipHeading {
+    position: relative;
+  }
+
+  a.routeSkipLink {
+    display: inline-block;
+    margin-left: -0.75em;
+    opacity: 0;
+    position: absolute;
+
+    &:before {
+      content: "<";
+      display: block;
     }
-    .routeSkipLink {
-        display: inline-block;
-        margin-left: -0.75em;
-        opacity: 0;
-        position: absolute;
-        text-decoration: none;
-    }
-    .routeSkipLink:before {
-        content: '⇽';
-        display: block;
-    }
-    .routeSkipLink:focus,
-    .routeSkipLink:hover {
-        opacity: 1;
-    }
-`
-const RouteHeading = ({level = 1, targetID, children}) => {
-    const Heading = `h${level}`;
-    return (
-        <Heading css={styles} className="routeSkipHeading">
-            <a href={`#${targetID}`}
-               id="skip-main"
-               className="routeSkipLink"
-               aria-label={`back to ${targetID}`}
-               title={`Skip to ${targetID}`}>
-            </a>
-            {children}
-        </Heading>
-    )
-}
-export default RouteHeading
+  }
+
+  .routeSkipLink:focus,
+  .routeSkipLink:hover {
+    opacity: 1;
+  }
+`;
+
+const RouteHeading = ({ level = 1, targetID, children }) => {
+  const Heading = `h${level}`;
+
+  return (
+    <Heading css={styles} className="routeSkipHeading">
+      <a
+        id="skip-link"
+        className="routeSkipLink"
+        aria-label={`skip to ${targetID}`}
+        href={`#${targetID}`}
+      ></a>
+      {children}
+    </Heading>
+  );
+};
+
+export default RouteHeading;
