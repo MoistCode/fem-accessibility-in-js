72ee43a1e7d44a5856593bc25715403e026c72e9
Tommy Pham <phamtommylong1991@gmail.com>
2019-10-23 11:39:23 -0700

    live region component

--- src/components/better/live-region.js
+++ src/components/better/live-region.js
@@ -1,11 +1,30 @@
 import React, { useState, useRef } from "react";
 
 const LiveRegion = () => {
+  const [message, setMessage] = useState("Nothing here");
+
+  const inputRef = useRef(null);
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    setMessage(inputRef.current.value);
+  };
+
   return (
     <div>
       <div role="status">
-        <p>Nothing here yet</p>
+        <p>{message}</p>
       </div>
+
+      <form onSubmit={submitHandler}>
+        <label>
+          Enter Text
+          <br />
+          <input type="text" ref={inputRef} />
+        </label>
+        <button type="submit">Start</button>
+      </form>
     </div>
   );
 };
