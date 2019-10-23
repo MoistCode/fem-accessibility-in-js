f8279c2d502cca380d1216731a343c67cb6ce36f
Tommy Pham <phamtommylong1991@gmail.com>
2019-10-23 10:20:33 -0700

    accessible dropdown

--- src/components/better/dropdown.js
+++ src/components/better/dropdown.js
@@ -4,15 +4,25 @@ import uuid from "uuid";
 import "./dropdown.scss";
 
 const Dropdown = ({ activatorText = "Dropdown", items = [] }) => {
+  const activatorRef = useRef(null);
+  const dropdownListRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+
   const clickHandler = e => {};
 
   return (
     <div className="dropdown-wrap">
-      <button aria-haspopup="true" aria-controls="dropdown1" onClick={clickHandler}>
+      <button
+        className=""
+        aria-haspopup="true"
+        aria-controls="dropdown1"
+        onClick={clickHandler}
+        ref={activatorRef}
+      >
         {activatorText}
       </button>
 
-      <ul id="dropdown1">
+      <ul id="dropdown1" ref={dropdownListRef}>
         {items.map(item => (
           <li key={item}>
             <a href={item.url}>{item.text}</a>
