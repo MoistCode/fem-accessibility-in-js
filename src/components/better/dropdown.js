6e6a6b54662c7d054d4850f02544ba6ca9792622
Tommy Pham <phamtommylong1991@gmail.com>
2019-10-23 10:14:23 -0700

    accessible dropdown

--- src/components/better/dropdown.js
+++ src/components/better/dropdown.js
@@ -1,15 +1,15 @@
-import React, { useState, useRef, useEffect } from "react"
-import uuid from "uuid"
+import React, { useState, useRef, useEffect } from "react";
+import uuid from "uuid";
 
-import "./dropdown.scss"
+import "./dropdown.scss";
 
-const Dropdown = ({ }) => {
-    return (
-        <div
-            className="dropdown-wrap"
-        >
-
-        </div>
-    )
-}
-export default Dropdown
+const Dropdown = ({ activatorText = "Dropdown", items = []}) => {
+  return (
+    <div className="dropdown-wrap">
+        <button>
+            {activatorText}
+        </button>
+    </div>
+  );
+};
+export default Dropdown;
